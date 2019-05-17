0df8c8c9f3a9151c32ee7dba1df60a298e6695ec
daomanhlinh97 <39149996+daomanhlinh97@users.noreply.github.com>
2019-05-17 17:48:23 +0700

    cấu trúc thư mục

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+
+app.engine('hbs', exphbs({
+  layoutsDir: 'views/_layouts',
+  defaultLayout: 'main.hbs'
+}));
+app.set('view engine', 'hbs');
+
+app.use(require('./middlewares/locals.mdw'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.use('/categories', require('./routes/category.route'))
+app.use('/admin/categories', require('./routes/admin/category.route'))
+
+app.listen(3000, () => {
+  console.log('server is running at http://localhost:3000');
+})
